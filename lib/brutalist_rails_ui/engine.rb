7b1988db9517b9b7565d3c0145387d4c6c90a5e3
Emerson <emerson@Informations-MacBook-Pro-2.local>
2026-05-13 07:30:29 -0700

    Fix helper inclusion: use config.to_prepare instead of on_load, remove isolate_namespace

--- lib/brutalist_rails_ui/engine.rb
+++ lib/brutalist_rails_ui/engine.rb
@@ -1,11 +1,7 @@
 module BrutalistRailsUi
   class Engine < ::Rails::Engine
-    isolate_namespace BrutalistRailsUi
-
-    initializer "brutalist_rails_ui.helpers" do
-      ActiveSupport.on_load(:action_view) do
-        include BrutalistRailsUi::Helpers
-      end
+    config.to_prepare do
+      ActionView::Base.include BrutalistRailsUi::Helpers
     end
   end
 end
